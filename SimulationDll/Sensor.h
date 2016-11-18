1810a00d4ba4cb33d35eb345c88510fd965477aa
dlut414 <hfy8373@gmail.com>
2016-11-18 19:26:22 +0900

    record Shear force on the boundary

--- SimulationDll/Sensor.h
+++ SimulationDll/Sensor.h
@@ -23,44 +23,62 @@ namespace SIM {
 		Sensor(Particle<R,2,Der>* _ptr) : ptr(_ptr) {
 			pos.clear();	vel.clear();	pres.clear(); gd.clear();
 			const auto& pt = ptr->derived();
-			for (int p = 0; p < pt.np; p++) {
-				if (IS(pt.bdc[p], T_DIRICHLET1)) {
-					id.push_back(p); gd.push_back(Vec::Zero());
-				}
-			}
-			for (int p = 0; p < pt.np; p++) {
-				if (IS(pt.bdc[p], T_DIRICHLET0)) {
-					id.push_back(p); gd.push_back(Vec::Zero());
-				}
-			}
+			//for (int p = 0; p < pt.np; p++) {
+			//	if (IS(pt.bdc[p], T_DIRICHLET1)) {
+			//		id_nu.push_back(p); gd.push_back(Vec::Zero());
+			//	}
+			//}
+			//for (int p = 0; p < pt.np; p++) {
+			//	if (IS(pt.bdc[p], T_DIRICHLET0)) {
+			//		id_nu.push_back(p); gd.push_back(Vec::Zero());
+			//	}
+			//}
+			//for (int p = 0; p < pt.np; p++) {
+			//	if (pt.type[p] == BD1) id_shear.push_back(p);
+			//}
 		}
 		~Sensor() {}
 
 		void operator >> (const std::string& str) {
-			interpolateVelocity();
-			std::ofstream file("./out/s" + str + ".out", std::ofstream::out);
-			for (auto s = 0; s < pos.size(); s++) {
-				for (auto d = 0; d < 2; d++) {
-					file << std::setprecision(6) << std::scientific << pos[s][d] << " ";
-				}
-				for (auto d = 0; d < 2; d++) {
-					file << std::setprecision(6) << std::scientific << vel[s][d] << " ";
-				}
-				file << std::endl;
-			}
-			file.close();
-			computeNusselt();
+			std::ofstream file;
+			//interpolateVelocity();
+			//file.open("./out/s" + str + ".out", std::ofstream::out);
+			//for (auto s = 0; s < pos.size(); s++) {
+			//	for (auto d = 0; d < 2; d++) {
+			//		file << std::setprecision(6) << std::scientific << pos[s][d] << " ";
+			//	}
+			//	for (auto d = 0; d < 2; d++) {
+			//		file << std::setprecision(6) << std::scientific << vel[s][d] << " ";
+			//	}
+			//	file << std::endl;
+			//}
+			//file.close();
+			//computeNusselt();
+			//const auto& pt = ptr->derived();
+			//file.open("./out/nu" + str + ".out", std::ofstream::out);
+			//for (auto s = 0; s < id_nu.size(); s++) {
+			//	auto p = id_nu[s];
+			//	for (auto d = 0; d < 2; d++) {
+			//		file << std::setprecision(6) << std::scientific << pt.pos[d][p] << " ";
+			//	}
+			//	for (auto d = 0; d < 2; d++) {
+			//		file << std::setprecision(6) << std::scientific << gd[s][d] << " ";
+			//	}
+			//	file << std::endl;
+			//}
+			//file.close();
+			file.open("./out/Shear" + str + ".out", std::ofstream::out);
 			const auto& pt = ptr->derived();
-			file.open("./out/nu" + str + ".out", std::ofstream::out);
-			for (auto s = 0; s < id.size(); s++) {
-				auto p = id[s];
-				for (auto d = 0; d < 2; d++) {
-					file << std::setprecision(6) << std::scientific << pt.pos[d][p] << " ";
+			for (int p = 0; p < pt.np; p++) {
+				if (pt.type[p] == BD1) {
+					Vec grad = pt.Grad(pt.vel[0].data(), p);
+					R shear = -grad.dot(pt.bdnorm[p]);
+					for (auto d = 0; d < 2; d++) {
+						file << std::setprecision(6) << std::scientific << pt.pos[d][p] << " ";
+					}
+					file << std::setprecision(6) << std::scientific << shear;
+					file << std::endl;
 				}
-				for (auto d = 0; d < 2; d++) {
-					file << std::setprecision(6) << std::scientific << gd[s][d] << " ";
-				}
-				file << std::endl;
 			}
 			file.close();
 			std::cout << " Writing Sensor. done " << std::endl;
@@ -105,13 +123,23 @@ namespace SIM {
 		}
 		void computeNusselt() {
 			const auto& pt = ptr->derived();
-			for (unsigned s = 0; s < id.size(); s++) {
-				gd[s] = pt.Grad(pt.temp.data(), id[s]);
+			for (unsigned s = 0; s < id_nu.size(); s++) {
+				gd[s] = pt.Grad(pt.temp.data(), id_nu[s]);
+			}
+		}
+		void computeShearX() {
+			const auto& pt = ptr->derived();
+			for (unsigned p = 0; p < pt.np; p++) {
+				if (pt.type[p] == BD1) {
+					Vec grad = pt.Grad(pt.vel[0].data(), p);
+					R shear = -grad.dot(pt.norm[p]);
+				}
 			}
 		}
 
 	public:
-		std::vector<int> id;
+		std::vector<int> id_nu;
+		std::vector<int> id_shear;
 		std::vector<Vec> pos;
 		std::vector<Vec> vel;
 		std::vector<Vec> gd;
